407e1928cae9a06feb619ffe46835992f3feec65
Yuying <70069160+yuyhong23@users.nonreply.github.com>
2020-12-09 13:13:20 -0800

    Rearranged tables so it won't give me primary key constrain error and import error. Updated the png too.

--- EmployeeSQL/ERD_export.sql
+++ EmployeeSQL/ERD_export.sql
@@ -6,71 +6,74 @@
 -- To reset the sample schema, replace everything with
 -- two dots ('..' - without quotes).
 
-CREATE TABLE "Departments" (
-    "Dept_No" VARCHAR(4)   NOT NULL,
-    "Dept_Name" VARCHAR(20)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "Dept_No"
-     )
-);
-
-CREATE TABLE "Department_Employees" (
-    "Emp_No" INT   NOT NULL,
-    "Dept_No" VARCHAR(4)   NOT NULL,
-    CONSTRAINT "pk_Department_Employees" PRIMARY KEY (
-        "Emp_No"
-     )
-);
-
-CREATE TABLE "Department_Managers" (
-    "Emp_No" INT   NOT NULL,
-    "Dept_No" VARCHAR(4)   NOT NULL,
-    CONSTRAINT "pk_Department_Managers" PRIMARY KEY (
-        "Emp_No"
+CREATE TABLE "Titles" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
      )
 );
 
 CREATE TABLE "Employees" (
-    "Emp_No" INT   NOT NULL,
-    "Emp_Title_ID" VARCHAR(5)   NOT NULL,
-    "Birth_Date" DATE   NOT NULL,
-    "First_Name" VARCHAR(20)   NOT NULL,
-    "Last_Name" VARCHAR(20)   NOT NULL,
-    "Sex" VARCHAR(1)   NOT NULL,
-    "Hire_Date" DATE   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(10)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(20)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "Emp_No","Emp_Title_ID"
+        "emp_no"
      )
 );
 
 CREATE TABLE "Salaries" (
-    "Emp_No" INT   NOT NULL,
-    "Salary" INT   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL,
     CONSTRAINT "pk_Salaries" PRIMARY KEY (
-        "Emp_No"
+        "emp_no","salary"
      )
 );
 
-CREATE TABLE "Titles" (
-    "Title_ID" VARCHAR(5)   NOT NULL,
-    "Title" VARCHAR(20)   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "Title_ID"
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
      )
 );
 
-ALTER TABLE "Department_Employees" ADD CONSTRAINT "fk_Department_Employees_Dept_No" FOREIGN KEY("Dept_No")
-REFERENCES "Departments" ("Dept_No");
+CREATE TABLE "Department_Employees" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_Department_Employees" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
 
-ALTER TABLE "Department_Managers" ADD CONSTRAINT "fk_Department_Managers_Dept_No" FOREIGN KEY("Dept_No")
-REFERENCES "Departments" ("Dept_No");
+CREATE TABLE "Department_Managers" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_Department_Managers" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_Emp_No" FOREIGN KEY("Emp_No")
-REFERENCES "Department_Employees" ("Emp_No");
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_Emp_No" FOREIGN KEY("Emp_No")
-REFERENCES "Department_Employees" ("Emp_No");
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
 
-ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_Title_ID" FOREIGN KEY("Title_ID")
-REFERENCES "Employees" ("Emp_Title_ID");
+ALTER TABLE "Department_Employees" ADD CONSTRAINT "fk_Department_Employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Department_Employees" ADD CONSTRAINT "fk_Department_Employees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Department_Managers" ADD CONSTRAINT "fk_Department_Managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Department_Managers" ADD CONSTRAINT "fk_Department_Managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
 
